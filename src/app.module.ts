d3e5850db4ce36539bafe174b5c5783504634659
bernardo haab <bernardohaab@gmail.com>
2022-02-08 23:12:14 -0300

    Set up docker configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://roo:root@db/analytic?authSource=admin', {
+      useNewUrlParser: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
